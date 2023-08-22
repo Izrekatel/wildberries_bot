f754db45a4f673b6c931ea75bbb156dd9996bd8c
Izrekatel <gelrudboris@gmail.com>
2023-08-22 12:26:10 +0500

    add new models, urls, subscriptions and other stuffs

--- bot/constants/constant.py
+++ bot/constants/constant.py
@@ -4,8 +4,8 @@ TELEGRAM_CHANEL_SUBSCRIBE = "https://t.me/dbfsfg"
 # После получения доступа к каналу подписки с правами админа
 # TELEGRAM_CHANEL_SUBSCRIBE = 'https://t.me/mpexperts'
 
-# BACKEND_URL = "nginx"
-BACKEND_URL = "localhost"
+BACKEND_URL = "nginx"
+# BACKEND_URL = "localhost"
 # if you start local telegram_bot change BACKEND_URL=localhost
 
 FREQUENCY_URL = f"http://{BACKEND_URL}/api/frequency/"
